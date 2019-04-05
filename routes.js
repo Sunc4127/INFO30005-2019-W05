e021285ecd8fd7185466a05358e9e3ff709d4541
Sunc4127 <42020175+Sunc4127@users.noreply.github.com>
2019-04-06 10:00:50 +1100

    Delete routes.js

--- routes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var controller = require('../controllers/controller.js');
-
-// Create new cafe
-router.post('/api',controller.createUsers);
-
-module.exports = router;
